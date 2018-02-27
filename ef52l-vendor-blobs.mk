27606550fae37b2315986434ae7ca1844ba1a687
Remilia Scarlet <remilia.1505@gmail.com>
2018-02-27 17:51:24 +0000

    ef52l: Import camera blobs
    Change-Id: I1a872dfeefbf1ccc4bcfa0638c1736a064906957
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- ef52l-vendor-blobs.mk
+++ ef52l-vendor-blobs.mk
@@ -14,3 +14,7 @@
 
 # Inherit from Pantech msm8960-common vendor
 $(call inherit-product, vendor/pantech/msm8960-common/msm8960-common-vendor-blobs.mk)
+
+PRODUCT_COPY_FILES += \
+    vendor/pantech/ef52l/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
+    vendor/pantech/ef52l/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so
